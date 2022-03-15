e642558df5448149228732f0702326149d1a613a
Kragstein <kragstein@protonmail.com>
2022-03-15 13:08:30 +0100

    Adding svg icons for settings, reload and numero

--- numero.js
+++ numero.js
@@ -185,15 +185,28 @@ this.numero.game = function (retValue) {
       }
       #game-status {
         padding: 0 1em 0;
+        display: grid;
+        justify-content: center;
+      }
+      button.icon {
+        background: none;
+        border: none;
+        cursor: pointer;
+        padding: 0 4px;
       }
     </style>
     <header>
       <div class="menu-left">
-        N
+        <game-icon icon="numero"></game-icon>
       </div>
       <div class="title">Numero</div>
       <div class="menu-right">
-        ⚙️
+        <button id="reload-button" class="icon" tabindex="-1">
+          <game-icon icon="reload"></game-icon>
+        </button>
+        <button id="settings-button" class="icon" tabindex="-1">
+					<game-icon icon="settings"></game-icon>
+				</button>
       </div>
     </header>
     <div id="game">
@@ -298,7 +311,7 @@ this.numero.game = function (retValue) {
           if (parseInt(this.valResDiv.innerHTML, 10) === this.valRes) {
             console.log("RIGHT");
             this.gameStatus = "ENDEDRIGHT";
-            this.gameStatusDiv.innerHTML = "WIN !";
+            this.gameStatusDiv.innerHTML = "WIN !" ;
           } else {
             console.log("WRONG");
           }
@@ -325,6 +338,99 @@ this.numero.game = function (retValue) {
     return returnFunction;
   }(SomethingElement(HTMLElement));
   customElements.define("numero-root", numeroRoot);
+
+  // icons
+
+  var iconSizes = {
+    settings: "0 0 45 45",
+    reload: "0 0 500 500",
+    numero: "100 100 800 700",
+  };
+
+  var iconPaths = {
+    settings: `
+    M43.454,18.443h-2.437c-0.453-1.766-1.16-3.42-2.082-4.933l1.752-1.756
+    c0.473-0.473,0.733-1.104,0.733-1.774 c0-0.669-0.262-1.301-0.733-1.773
+    l-2.92-2.917c-0.947-0.948-2.602-0.947-3.545-0.001l-1.826,1.815
+    C30.9,6.232,29.296,5.56,27.529,5.128V2.52c0-1.383-1.105-2.52-2.488-2.52
+    h-4.128c-1.383,0-2.471,1.137-2.471,2.52v2.607  c-1.766,0.431-3.38,1.104
+    -4.878,1.977l-1.825-1.815c-0.946-0.948-2.602-0.947-3.551-0.001L5.27,
+    8.205 C4.802,8.672,4.535,9.318,4.535,9.978c0,0.669,0.259,1.299,0.733,
+    1.772l1.752,1.76c-0.921,1.513-1.629,3.167-2.081,4.933H2.501 C1.117,
+    18.443,0,19.555,0,20.935v4.125c0,1.384,1.117,2.471,2.501,2.471h2.438
+    c0.452,1.766,1.159,3.43,2.079,4.943l-1.752,1.763 c-0.474,0.473-0.734,
+    1.106-0.734,1.776s0.261,1.303,0.734,1.776l2.92,2.919c0.474,0.473,1.103,
+    0.733,1.772,0.733 s1.299-0.261,1.773-0.733l1.833-1.816c1.498,0.873,
+    3.112,1.545,4.878,1.978v2.604c0,1.383,1.088,2.498,2.471,2.498h4.128
+    c1.383,0,2.488-1.115,2.488-2.498v-2.605c1.767-0.432,3.371-1.104,4.869
+    -1.977l1.817,1.812c0.474,0.475,1.104,0.735,1.775,0.735 c0.67,0,1.301
+    -0.261,1.774-0.733l2.92-2.917c0.473-0.472,0.732-1.103,0.734-1.772
+    c0-0.67-0.262-1.299-0.734-1.773l-1.75-1.77 c0.92-1.514,1.627-3.179,
+    2.08-4.943h2.438c1.383,0,2.52-1.087,2.52-2.471v-4.125C45.973,19.555,
+    44.837,18.443,43.454,18.443z M22.976,30.85c-4.378,0-7.928-3.517-7.928
+    -7.852c0-4.338,3.55-7.85,7.928-7.85c4.379,0,7.931,3.512,7.931,7.85
+    C30.906,27.334,27.355,30.85,22.976,30.85z
+    `,
+    reload: `
+    M268.175,488.161c98.2-11,176.9-89.5,188.1-187.7c14.7-128.4-85.1-237.7-210.2
+    -239.1v-57.6c0-3.2-4-4.9-6.7-2.9 l-118.6,87.1c-2,1.5-2,4.4,0,5.9l118.6,87.1
+    c2.7,2,6.7,0.2,6.7-2.9v-57.5c87.9,1.4,158.3,76.2,152.3,165.6c-5.1,76.9-67.8
+    ,139.3-144.7,144.2c-81.5,5.2-150.8-53-163.2-130c-2.3-14.3-14.8-24.7-29.2
+    -24.7c-17.9,0-31.9,15.9-29.1,33.6 C49.575,418.961,150.875,501.261,268.175
+    ,488.161z`,
+    numero: `
+    M508 280h-63.3c-3.3 0-6 2.7-6 6v340.2H433L197.4 282.6c-1.1-1.6-3-2.6-4.9
+    -2.6H126c-3.3 0-6 2.7-6 6v464c0 3.3 2.7 6 6 6h62.7c3.3 0 6-2.7 6-6V405.1
+    h5.7l238.2 348.3c1.1 1.6 3 2.6 5 2.6H508c3.3 0 6-2.7 6-6V286c0-3.3-2.7-6
+    -6-6zM886 693H582c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8h304c4.4 0 8-3.6 8-8
+    v-48c0-4.4-3.6-8-8-8zM733.8 630c52.9 0 95.2-17.2 126.2-51.7 29.4-32.9 44
+    -75.8 44-128.8 0-53.1-14.6-96.5-44-129.3-30.9-34.8-73.2-52.2-126.2-52.2
+    -53.7 0-95.9 17.5-126.3 52.8-29.2 33.1-43.4 75.9-43.4 128.7 0 52.4 14.3
+    95.2 43.5 128.3 30.6 34.7 73 52.2 126.2 52.2z m-71.5-263.7c16.9-20.6 40.3
+    -30.9 71.4-30.9 31.5 0 54.8 9.6 71 29.1 16.4 20.3 24.9 48.6 24.9 84.9 0
+    36.3-8.4 64.1-24.8 83.9-16.5 19.4-40 29.2-71.1 29.2-31.2 0-55-10.3-71.4
+    -30.4-16.3-20.1-24.5-47.3-24.5-82.6 0.1-35.8 8.2-63 24.5-83.2z`,
+  };
+
+  var iconElement = document.createElement("template");
+  iconElement.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" height="24"
+         viewBox="0 0 50 50" width="24">
+      <path fill=var(--color-tone-1) />
+    </svg>
+  `;
+
+  var icon = function(htmlElement) {
+    setPrototype(returnFunction, htmlElement);
+    var element = constructElement(returnFunction);
+
+    function returnFunction() {
+      var e;
+      isInstanceOf(this, returnFunction);
+      (e = element.call(this)).attachShadow({ mode: "open" });
+      return e;
+    }
+
+    addKeyFunction(returnFunction , [{
+      key: "connectedCallback",
+      value: function() {
+        this.shadowRoot.appendChild(iconElement.content.cloneNode(!0));
+        var e = this.getAttribute("icon");
+        this.shadowRoot.querySelector("path").setAttribute("d", iconPaths[e]);
+        this.shadowRoot.querySelector("svg").setAttribute("viewBox", iconSizes[e]);
+
+        if (e === "reload") {
+          this.addEventListener("click", function(a) {
+            console.log("reload");
+          });
+        }
+      }
+    }]);
+
+    return returnFunction;
+  }(SomethingElement(HTMLElement));
+
+  customElements.define("game-icon", icon);
 
   // Function magic to make new tags
 
